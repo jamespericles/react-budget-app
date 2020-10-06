af6a91f13713cf6bf8f09cc46288179db1557d0c
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-10-05 21:25:34 -0400

    created a redux store with authentication reducer

---
+++ src/reducers/auth.js
@@ -0,0 +1,16 @@
+// Utilized for handling signing in and out.
+
+import { SIGN_IN, SIGN_OUT } from "../utils/constants";
+
+const authReducer = (state = {}, action) => {
+  switch (action.type) {
+    case SIGN_IN:
+      return action.user;
+    case SIGN_OUT:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
