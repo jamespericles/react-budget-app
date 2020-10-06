5d43ec30eef9c391f5d73f6d850769d5cf4c7c74
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-10-05 21:26:05 -0400

    created a redux store with authentication reducer

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+// Placing shared constants in one file so that whenever they are changed here, they are effectively changed in all places they are used
+
+export const SIGN_IN = "SIGN_IN";
+export const SIGN_OUT = "SIGN_OUT";
