59b9b59bb41840ca242dad207728e1298c4f66dd
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-10-08 13:12:05 -0400

    updated server/index to allow for cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const authRoute = require("./routes/auth");
 require("dotenv").config();
 
@@ -6,8 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use(authRoute);
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`server started on port ${PORT}`);
 });
